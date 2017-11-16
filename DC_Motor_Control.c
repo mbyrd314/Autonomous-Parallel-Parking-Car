072ad269692dcfab3d69b30a42c702a50d595dc9
Samvruta Tumuluru <tsamvrutha@gmail.com>
2017-11-16 06:51:38 +0000

    working c function to control motors via ZMQ

---
+++ DC_Motor_Control.c
@@ -0,0 +1,61 @@
+#include "DC_Motor_Control.h"
+
+
+void forward(int speed, int t, struct zmq_socket *p_socket){
+    char message[MSG_SIZE];
+    sprintf(message,"0,%d,%d", speed, t);
+    send_instr(message,  p_socket);
+}
+
+void backward(int speed, int t, struct zmq_socket *p_socket){
+    char message[MSG_SIZE];
+    sprintf(message,"1,%d,%d", speed, t);
+    send_instr(message,  p_socket);
+}
+void left(int speed, int t, struct zmq_socket *p_socket){
+    char message[MSG_SIZE];
+    sprintf(message,"2,%d,%d", speed, t);
+    send_instr(message,  p_socket);
+}
+
+void back_left(int speed, int t, struct zmq_socket *p_socket){
+    char message[MSG_SIZE];
+    sprintf(message,"3,%d,%d", speed, t);
+    send_instr(message, p_socket);
+}
+
+void right(int speed, int t, struct zmq_socket *p_socket){
+    char message[MSG_SIZE];
+    sprintf(message,"4,%d,%d", speed, t);
+    send_instr(message, p_socket);
+}
+
+void back_right(int speed, int t, struct zmq_socket *p_socket){
+    char message[MSG_SIZE];
+    sprintf(message,"5,%d,%d", speed, t);
+    send_instr(message, p_socket);
+}
+
+void stop(struct zmq_socket *p_socket){
+    //int speed = 0;
+    //int t = 0;
+    char message[MSG_SIZE];
+    sprintf(message,"6,0,0");
+    send_instr(message, p_socket);
+}
+
+int main(){
+    struct zmq_socket zsocket;
+    init_socket(&zsocket);
+    forward(20,1,&zsocket);
+    stop(&zsocket);
+    stop(&zsocket);
+    stop(&zsocket);
+    stop(&zsocket);
+    stop(&zsocket);
+    backward(20,1,&zsocket);
+    back_right(20,1,&zsocket);
+    right(20,1,&zsocket);
+    left(20,1,&zsocket);
+    back_left(20,1,&zsocket);
+}
---
+++ DC_Motor_Control.h
@@ -0,0 +1,48 @@
+#ifndef DC_Motor_Control_H_
+#define DC_Motor_Control_H_
+
+#include "ZMQC.h"
+/**
+    * Moves car forward for specified amount of time with set speed
+    *@param duty cycle of motors to control speed must be between 0-100
+    *@param zmq_socket struct pointer
+**/
+void forward(int speed, int t, struct zmq_socket *p_socket);
+
+/**
+    * Moves car backward for specified amount of time with set speed
+    *@param duty cycle of motors to control speed must be between 0-100
+    *@param zmq_socket struct pointer
+**/
+void backward(int speed, int t, struct zmq_socket *p_socket);
+/**
+    * Moves car left for specified amount of time with set speed
+    *@param duty cycle of motors to control speed must be between 0-100
+    *@param zmq_socket struct pointer
+**/
+void left(int speed, int t, struct zmq_socket *p_socket);
+/**
+    * Moves car back and left for specified amount of time with set speed
+    *@param duty cycle of motors to control speed must be between 0-100
+    *@param zmq_socket struct pointer
+**/
+void back_left(int speed, int t, struct zmq_socket *p_socket);
+/**
+    * Moves car right for specified amount of time with set speed
+    *@param duty cycle of motors to control speed must be between 0-100
+    *@param zmq_socket struct pointer
+**/
+void right(int speed, int t, struct zmq_socket *p_socket);
+/**
+    * Moves car back and right for specified amount of time with set speed
+    *@param duty cycle of motors to control speed must be between 0-100
+    *@param zmq_socket struct pointer
+**/
+void back_right(int speed, int t, struct zmq_socket *p_socket);
+/**
+    *Stops car
+    *@param zmq_socket struct pointer
+**/
+void stop(struct zmq_socket *p_socket);
+
+#endif /*DC_Motor_Control_H_*/
